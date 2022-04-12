3fc0fb3074598f5027a34a17ee6668ecbbd8aee2
Akran God <narka969@gmail.com>
2022-04-12 16:21:22 +0800

    Hi ganzo 1st commit

---
+++ lib/screens/otp/components/body.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/size_config.dart';
+
+import 'otp_form.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: SizeConfig.screenHeight * 0.05),
+              Text(
+                "OTP баталгаажуулалт",
+                style: headingStyle,
+              ),
+              Text("Бид +976 9949 **** -руу баталгаажуулах код илгээсэн"),
+              buildTimer(),
+              OtpForm(),
+              SizedBox(height: SizeConfig.screenHeight * 0.1),
+              GestureDetector(
+                onTap: () {
+                  // OTP code resend
+                },
+                child: Text(
+                  "Дахин баталгаажуулах код явуулах",
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row buildTimer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Код ашиглах хугацаа "),
+        TweenAnimationBuilder(
+          tween: Tween(begin: 30.0, end: 0.0),
+          duration: Duration(seconds: 30),
+          builder: (_, dynamic value, child) => Text(
+            "00:${value.toInt()}",
+            style: TextStyle(color: kPrimaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
